03e33e15b7a5153b37ab9678e88bd68640c19d66
Adek Rahmat Kurniawan <adeeerk@gmail.com>
2024-12-06 13:36:35 +0700

    LIVE CODE

--- 1.js
+++ 1.js
@@ -1,25 +1,24 @@
 function getStartUp(arr) {
-  let array = []
-  let str = ""
   // Write your code here
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "-") {
-      array.push(str)
-      str = ""
+  let startupArr = []
+  let init = []
+  for (let i = 0; i < arr.length; i++){
+    let startup = ""
+    let last = arr[i][arr[i].length - 1]
+    init.push(last)
+    for (let j = 0; j < arr[i].length - 2; j++) {
+      startup += arr[i][j]
     }
-    if (str) {
-      array.push(str)
-    }
+    startupArr.push(startup)
   }
-  return array
+  return startupArr
 }
-
 //Test Case
 console.log(getStartUp(['Bikilipik-E', 'Tikit.cim-T', 'Ilidiktir-H', 'TiniHib-A']))
 // [ 'Bikilipik', 'Tikit.cim', 'Ilidiktir', 'TiniHib' ]
 
 // console.log(getStartUp(['Lizidi-E', 'Iirbnb-T', 'TikingSiyir.ci-A', 'PriSihit-H', 'DiktirSihit-H', 'eFishiry-A', 'babaAli-E', 'eMeat-A']))
-// // [ 'babaAli', 'Iirbnb', 'DiktirSihit', 'TikingSiyir.ci' ]
+// [ 'babaAli', 'Iirbnb', 'DiktirSihit', 'TikingSiyir.ci' ]
 
 // console.log(getStartUp(['Tikipidia-E', 'Triviliki-T', 'Hilidic-H', 'Siyirbix-A', 'Shipee-E', 'Bhinniki-E', 'BleBle.cim-E', 'PigiPigi-T', 'tomazo-A']))
 // // [ 'BleBle.cim', 'Triviliki', 'Hilidic', 'Siyirbix' ]
