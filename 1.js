5a2eaeb86c0475dd64a638c4d48328977e4bd332
Adek Rahmat Kurniawan <adeeerk@gmail.com>
2024-12-06 18:20:31 +0700

    LIVE CODE

--- 1.js
+++ 1.js
@@ -1,33 +1,89 @@
 function getStartUp(arr) {
   // Write your code here
-  let startUp = []
-  let last = []
-  // looping untuk setiap nama startup
-  for (let i = 0; i < arr.length; i++) {
-    temp = arr[i] // Mengambil nama startup
-    let modified = "" //variabel untuk menyimpan nama yang sudah berkurang 2 huruf belakang
-    let lastChar = temp[temp.length - 1] // menyimpan satu kata terakhir
+  let bidangE = '';
+  let bidangT = '';
+  let bidangH = '';
+  let bidangA = '';
 
-    for (let j = 0; j < temp.length - 2; j++) {
-      modified += temp[j]
+  // Pisahkan startup berdasarkan bidangnya
+  for (let i = 0; i < arr.length; i++) {
+    let startup = '';
+    let bidang = '';
+    let separatorFound = false;
+
+    // Loop manual untuk memisahkan nama startup dan bidang
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === '-') {
+        separatorFound = true;
+      } else if (separatorFound) {
+        bidang += arr[i][j]; // Tambahkan karakter ke bidang
+      } else {
+        startup += arr[i][j]; // Tambahkan karakter ke nama startup
+      }
     }
-    last.push(lastChar)
-    startUp.push(modified)
+
+    // Tentukan startup terpanjang untuk setiap bidang
+    if (bidang === 'E' && startup.length > bidangE.length) {
+      bidangE = startup;
+    } else if (bidang === 'T' && startup.length > bidangT.length) {
+      bidangT = startup;
+    } else if (bidang === 'H' && startup.length > bidangH.length) {
+      bidangH = startup;
+    } else if (bidang === 'A' && startup.length > bidangA.length) {
+      bidangA = startup;
+    }
   }
-  console.log(last)
-  return startUp
+
+  // Kembalikan hasil
+  return [bidangE, bidangT, bidangH, bidangA];
 }
 //Test Case
-console.log(getStartUp(['Bikilipik-E', 'Tikit.cim-T', 'Ilidiktir-H', 'TiniHib-A']))
+console.log(
+  getStartUp(["Bikilipik-E", "Tikit.cim-T", "Ilidiktir-H", "TiniHib-A"])
+);
 // [ 'Bikilipik', 'Tikit.cim', 'Ilidiktir', 'TiniHib' ]
 
-console.log(getStartUp(['Lizidi-E', 'Iirbnb-T', 'TikingSiyir.ci-A', 'PriSihit-H', 'DiktirSihit-H', 'eFishiry-A', 'babaAli-E', 'eMeat-A']))
+console.log(
+  getStartUp([
+    "Lizidi-E",
+    "Iirbnb-T",
+    "TikingSiyir.ci-A",
+    "PriSihit-H",
+    "DiktirSihit-H",
+    "eFishiry-A",
+    "babaAli-E",
+    "eMeat-A",
+  ])
+);
 // [ 'babaAli', 'Iirbnb', 'DiktirSihit', 'TikingSiyir.ci' ]
 
-console.log(getStartUp(['Tikipidia-E', 'Triviliki-T', 'Hilidic-H', 'Siyirbix-A', 'Shipee-E', 'Bhinniki-E', 'BleBle.cim-E', 'PigiPigi-T', 'tomazo-A']))
+console.log(
+  getStartUp([
+    "Tikipidia-E",
+    "Triviliki-T",
+    "Hilidic-H",
+    "Siyirbix-A",
+    "Shipee-E",
+    "Bhinniki-E",
+    "BleBle.cim-E",
+    "PigiPigi-T",
+    "tomazo-A",
+  ])
+);
 // // [ 'BleBle.cim', 'Triviliki', 'Hilidic', 'Siyirbix' ]
 
-console.log(getStartUp(['FoGoods-A', 'FoMarts-E', 'Lazora-E', 'eFruit-A', 'FoHealth-H', 'FoTravel-T', 'HaiDoc-H', 'ticket.net-T']))
+console.log(
+  getStartUp([
+    "FoGoods-A",
+    "FoMarts-E",
+    "Lazora-E",
+    "eFruit-A",
+    "FoHealth-H",
+    "FoTravel-T",
+    "HaiDoc-H",
+    "ticket.net-T",
+  ])
+);
 // // [ 'FoMarts', 'ticket.net', 'FoHealth', 'FoGoods' ]
 
-module.exports = getStartUp
+module.exports = getStartUp;
